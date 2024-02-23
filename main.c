f60736ffbab018d580f018f2ab7527af66ea37d5
Cristian David Araujo Araujo <51882823+Cristian-David-Araujo@users.noreply.github.com>
2024-02-23 00:51:39 +0000

    My version of "Hello World"

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 /*!
 \brief this is the first program in where we will start the program
-\author
+\author Cristian David Araujo Araujo, Maverick Sossa Tobon
 */
 
 // Libraries
 #include <stdio.h>
 
 int main(){
-    printf("Hello, World Mor!\n");
+    // Print Hello, World 10 times
+    for (int i = 0; i < 10; i++){
+        printf("Hello, World Mor!\n");
+    }
+
+    printf("That's all folks!\n");
+
     return 0;
 }
