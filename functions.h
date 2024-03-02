cccdb661ecc1c342bf466503f5a61a95a6ce07e0
MaverickST <80019104+MaverickST@users.noreply.github.com>
2024-03-02 15:21:37 +0000

    Add printing functions

--- functions.h
+++ functions.h
@@ -9,4 +9,8 @@ unsigned char* BitsToBytes(unsigned char* bitArray);
 unsigned char* BytesToBits(unsigned char* bytesArray);
 unsigned char* byteEncode(__uint16_t F[], __uint8_t d);
 __uint16_t* byteDecode(unsigned char* byteArray, __uint8_t d);
+
+void printArrayInt(__uint16_t* array, __uint32_t size);
+void printArrayChar(unsigned char* array, __uint32_t size);
+
 #endif
